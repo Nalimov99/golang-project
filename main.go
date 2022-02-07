f0f03d5fea958e07c643aae49646e890eda3d749
Nalimov <nalimvp@gmail.com>
2022-02-07 15:44:48 +0300

    gracefull shutdown

--- main.go
+++ main.go
@@ -1,18 +1,61 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-	h := http.HandlerFunc(Echo)
+	log.Println("main : Started")
+	defer log.Println("main : Completed")
 
-	if err := http.ListenAndServe("localhost:3020", h); err != nil {
-		fmt.Print("Error")
+	const timeout = 5 * time.Second
+
+	api := http.Server{
+		Addr:         "localhost:3020",
+		Handler:      http.HandlerFunc(Echo),
+		ReadTimeout:  timeout,
+		WriteTimeout: timeout,
+	}
+
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Printf("main : API listening on %s", api.Addr)
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("error : listening and serving: %s", err)
+
+	case <-shutdown:
+		log.Println("main : Start shutdown")
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		err := api.Shutdown(ctx)
+		if err != nil {
+			log.Printf("main : Gracefull shutdown not complete in %v : %v", timeout, err)
+			err = api.Close()
+		}
+
+		if err != nil {
+			log.Fatalf("main : Could not stop server gracefully : %v", err)
+		}
 	}
 }
 
 func Echo(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(time.Second)
 	fmt.Fprint(w, r.Method, " ", r.URL)
 }
